26b32165c1336ff7355bc927aee72cfd2968c3b6
Agrippino <agrippinorussiello@outlook.it>
2022-04-22 18:41:18 +0200

    Riprovo a pushare

---
+++ ListaAppuntamenti/Appuntamento.cs
@@ -0,0 +1,63 @@
+﻿
+
+namespace ListaAppuntamenti
+{
+    internal class Appuntamento
+    //variabili//
+    {
+        private protected string nome { get; set; }
+        private protected string localita { get; set; }
+
+        private protected DateTime dataAppuntamento = new DateTime();
+
+        private protected DateTime dataAttuale = DateTime.Now;
+
+        public Appuntamento(string nome, string localita, DateTime dataAppuntamento)
+        {
+            this.nome = nome;
+            this.localita = localita;
+            this.dataAppuntamento = dataAppuntamento;
+           
+
+            //verifichiamo de la data del'appuntamento sia pi vecchia di oggi 
+            if (dataAppuntamento < dataAttuale)
+            {
+                throw new Exception("La data è sbaglaita");
+            }
+        }
+
+        //metodi//
+        public virtual void ToString()
+        {
+            // string ConsoleWriteLine = "";
+
+            Console.WriteLine("---La tua agenda--- \n"); 
+            Console.WriteLine("Nome della persona con cui hai l'appuntamento" + this.nome + "\n");
+            Console.WriteLine("Località del tuo appuntamento" + this.localita + "\n");
+            Console.WriteLine("Data del tuo appuntamento" + this.dataAppuntamento + "\n");
+
+            //return formDellaAgenda;
+        }
+
+        public DateTime modificaLaData()
+        {
+                Console.WriteLine("Perfetto,qual è la nuova data ?");
+           
+            DateTime dataDaCambiare = DateTime.Parse(Console.ReadLine());
+                dataAppuntamento = dataDaCambiare;
+                if (dataAppuntamento < dataAttuale)
+            {
+                throw new Exception("La data è sbaglaita");
+            }
+            return dataAppuntamento;
+        }
+    }
+    }
+
+
+/*Console.WriteLine("Vuoi modificare l'appuntamento scirtto in passato? [sì/no]");
+string rispostaUtente = Console.ReadLine();
+switch (rispostaUtente)
+{
+    case "sì":*/
+
--- ListaAppuntamenti/Program.cs
+++ ListaAppuntamenti/Program.cs
@@ -1,1 +1,52 @@
-﻿
+﻿using ListaAppuntamenti;
+
+//creo una lista//
+List<Appuntamento> listaDeiMieiAppuntamenti = new List<Appuntamento>();
+
+Console.WriteLine("Quanti Appuntamenti vuoi inserire?");
+int numeroDiAppuntamenti = int.Parse(Console.ReadLine());
+
+
+for (int i = 0; i < numeroDiAppuntamenti; i++)
+{
+    Console.WriteLine("Ciao, sono il tuo aiutante per la tua agenda personale \n");
+    Console.WriteLine("Inserisci qui il nome della persona che incontrerai");
+    string nome = Console.ReadLine();
+    Console.WriteLine("Inserisci qui la località del tuo appuntamento");
+    string localita = Console.ReadLine();
+    
+    bool controlloData = false;
+    while (controlloData == false)
+    {
+        try
+        {
+            Console.WriteLine("Inserisci qui la data del tuo incontro in forma numerica");
+            DateTime data = DateTime.Parse(Console.ReadLine());
+
+            listaDeiMieiAppuntamenti.Add(new Appuntamento(nome, localita, data));
+
+            controlloData = true;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
+try
+{
+    Console.WriteLine("inserisci l'indice che vuoi cambiare");
+    int indiceAppuntamenti = int.Parse(Console.ReadLine());
+    listaDeiMieiAppuntamenti[indiceAppuntamenti].modificaLaData();
+} catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+
+
+foreach (Appuntamento ogniAppuntameto in listaDeiMieiAppuntamenti)
+{
+    ogniAppuntameto.ToString();
+}
+
+
